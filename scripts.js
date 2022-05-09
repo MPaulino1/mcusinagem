314a1de750b17609baacc078f63bdf108ac0a233
Manoel Paulino <mpaulino.me@gmail.com>
2022-05-09 20:12:05 -0300

    segundo commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const Modal = {
+    open(){
+        //open modal
+        //Add a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+    }
